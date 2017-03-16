0d0d3f4b2b2941697f5187023de5910f363650b4
Josiah R <josiahrentsch@gmail.com>
2017-03-16 09:13:09 -0700

    Added modules for flight delays and flight times.

---
+++ 2015_flight_analysis/flight_times.py
@@ -0,0 +1,10 @@
+"""
+This module contains routines for calculating and modelling the
+flight times of different airline flights from and to the same
+destinations.
+Contains routines to answer the following questions:
+    -Does a particular city/location and time/date have more
+    delays than other locations?
+    -Do different airlines have the same travel times to the
+    same locations?
+"""
