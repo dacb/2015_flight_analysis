e5d420be21daf4948b1d3c3322a66626145e95e4
Josiah R <josiahrentsch@gmail.com>
2017-03-16 09:04:56 -0700

    Added a main file for cleaner execution

---
+++ 2015_flight_analysis/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+UW-SEDS Capstone Project
+March 2017
+
+This program reads in the raw data and stores them into pandas dataframes
+
+Downloaded from Kaggle Project
+2015 Flight Delays and Cancellations
+
+There are three .csv files
+  15 airline codes and expansions
+  322 airport codes and expansions
+  5.8
+"""
+
+import read_raw_data as rd
+
+
+def main():
+    """ Main program """
+    # Set the relative path
+    rd.set_path()
+
+    # Data file paths
+    airline_path = "../../data/airlines.csv"
+    airports_path = "../../data/airports.csv"
+    flights_path = "../../data/flights.csv"
+
+    # Get pandas data frames for the data sets
+    airline_df = rd.get_data_frame(airline_path, )
+    airport_df = rd.get_data_frame(airports_path, )
+    params = {'ORIGIN_AIRPORT': str, 'DESTINATION_AIRPORT': str}
+    flights_df = rd.get_data_frame(flights_path, params)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
--- 2015_flight_analysis/read_raw_data.py
+++ 2015_flight_analysis/read_raw_data.py
@@ -1,28 +1,12 @@
 # read_raw_data_1.py
 """
-UW-SEDS Capstone Project
-March 2017
-
-This program reads in the raw data and stores them into pandas dataframes
-
-Downloaded from Kaggle Project
-2015 Flight Delays and Cancellations
-
-There are three .csv files
-  15 airline codes and expansions
-  322 airport codes and expansions
-  5.8 million flights
+File read and data frame utilities
 """
 
 import os
 import sys
 
 import pandas as pd
-
-# Data file paths
-AIRLINE_PATH = "../../data/airlines.csv"
-AIRPORTS_PATH = "../../data/airports.csv"
-FLIGHTS_PATH = "../../data/flights.csv"
 
 
 def set_path():
@@ -37,18 +21,12 @@ def get_data_frame(path, params=""):
     return pd.read_csv(path)
 
 
-set_path()
+# def get_data_frame_tuple():
+#     """returns tuples of data frames"""
+#     # Read the data into pandas data frames
+#     airline_df = get_data_frame(AIRLINE_PATH, )
+#     airport_df = get_data_frame(AIRPORTS_PATH, )
+#     params = {'ORIGIN_AIRPORT': str, 'DESTINATION_AIRPORT': str}
+#     flights_df = get_data_frame(FLIGHTS_PATH, params)
+#     return [airline_df, airport_df, flights_df]
 
-
-def get_data_frame_tuple():
-    """returns tuples of data frames"""
-    # todo: don't get the data this way, just call the get_data_frame routine
-    # Read the data into pandas data frames
-    airline_df = get_data_frame(AIRLINE_PATH, )
-    airport_df = get_data_frame(AIRPORTS_PATH, )
-    params = {'ORIGIN_AIRPORT': str, 'DESTINATION_AIRPORT': str}
-    flights_df = get_data_frame(FLIGHTS_PATH, params)
-    return [airline_df, airport_df, flights_df]
-
-
-get_data_frame_tuple()
